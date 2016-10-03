8395bd0abaecf200744f1bf6c311e698c5c53fc6
Eric Tzeng <etzeng@eecs.berkeley.edu>
2016-10-03 14:19:53 -0400

    Add grayscale option to ImageTransformer

--- mldata/transform.py
+++ mldata/transform.py
@@ -4,8 +4,9 @@ import skimage.transform
 
 class ImageTransformer(object):
 
-    def __init__(self, image_size=None):
+    def __init__(self, image_size=None, grayscale=False):
         self.image_size = image_size
+        self.grayscale = grayscale
 
     def preprocess_batch(self, batch):
         preprocessed = []
@@ -13,7 +14,14 @@ class ImageTransformer(object):
             preprocessed.append(self.preprocess(im))
         return np.stack(preprocessed, axis=0)
 
+    def rgb2y(self, im):
+        """Converts RGB to luminance, following BT.601."""
+        if im.shape[2] == 3:
+            return (im * [.299, .587, .114]).sum(axis=2)[:, :, np.newaxis]
+
     def preprocess(self, im):
+        if self.grayscale:
+            im = self.rgb2y(im)
         if self.image_size is not None:
             im = skimage.transform.resize(im, self.image_size,
                                           preserve_range=True)
