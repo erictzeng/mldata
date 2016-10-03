f33f9ded034404dfc5d21d3e316571d9a70b522c
Eric Tzeng <etzeng@eecs.berkeley.edu>
2016-10-03 13:49:29 -0400

    Implement SVHN dataset

---
+++ mldata/vision/svhn.py
@@ -0,0 +1,61 @@
+from __future__ import division
+
+import urlparse
+
+import numpy as np
+from scipy.io import loadmat
+
+import mldata.util
+from mldata.dataset import Dataset
+from mldata.dataset import DatasetGroup
+
+
+class CroppedSVHN(DatasetGroup):
+    """The Street View House Numbers Dataset.
+
+    This DatasetGroup corresponds to format 2, which consists of center-cropped
+    digits.
+
+    Homepage: http://ufldl.stanford.edu/housenumbers/
+
+    Images are 32x32 RGB images in the range [0, 1].
+    """
+
+    base_url = 'http://ufldl.stanford.edu/housenumbers/'
+
+    data_files = {
+            'train': 'train_32x32.mat',
+            'test': 'test_32x32.mat',
+            'extra': 'extra_32x32.mat',
+            }
+
+    def __init__(self, path=None, train_on_extra=False):
+        DatasetGroup.__init__(self, 'svhn_cropped', path=path)
+        self.train_on_extra = train_on_extra
+        self._load_datasets()
+
+    def download(self):
+        for filename in self.data_files.values():
+            url = urlparse.urljoin(self.base_url, filename)
+            dest = self.get_path(filename)
+            mldata.util.maybe_download(url, dest)
+
+    def _load_datasets(self):
+        abspaths = {name: self.get_path(path)
+                    for name, path in self.data_files.items()}
+        train_mat = loadmat(abspaths['train'])
+        train_images = train_mat['X'].transpose((3, 0, 1, 2))
+        train_labels = train_mat['y'].squeeze()
+        if self.train_on_extra:
+            extra_mat = loadmat(abspaths['extra'])
+            train_images = np.vstack((train_images,
+                                      extra_mat['X'].transpose((3, 0, 1, 2))))
+            train_labels = np.concatenate((train_labels,
+                                           extra_mat['y'].squeeze()))
+        train_images = train_images.astype(np.float32) / 255
+        test_mat = loadmat(abspaths['test'])
+        test_images = test_mat['X'].transpose((3, 0, 1, 2))
+        test_images = test_images.astype(np.float32) / 255
+        test_labels = test_mat['y'].squeeze()
+        self.train = Dataset(train_images, train_labels)
+        self.test = Dataset(test_images, test_labels)
