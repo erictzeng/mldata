0b699be5e89879de0989e68f80f44a72c5d88996
Eric Tzeng <etzeng@eecs.berkeley.edu>
2016-10-11 18:40:28 -0400

    Add label_mapping parameter to PairedDataset

--- mldata/general/paired.py
+++ mldata/general/paired.py
@@ -6,14 +6,17 @@ class PairedDataset(object):
     their labels.
     """
 
-    def __init__(self, dataset1, dataset2):
+    def __init__(self, dataset1, dataset2, label_mapping=None):
         self.dataset1 = dataset1
         self.dataset2 = dataset2
+        self.label_mapping = label_mapping if label_mapping is not None else {}
 
     def next_batch(self, n):
         images1, labels = self.dataset1.next_batch(n)
         images2 = []
         for im1, label in zip(images1, labels):
+            if label in self.label_mapping:
+                label = self.label_mapping[label]
             inds = np.where(self.dataset2.labels == label)[0]
             im2 = self.dataset2.get_image(np.random.choice(inds))
             images2.append(im2)
